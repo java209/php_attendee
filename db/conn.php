e5b15613150003d67698767a76b16394910c1fd9
Javannie Wright <kemmoy@live.com>
2021-11-22 21:59:34 -0500

    added remote mysql connection details

--- db/conn.php
+++ db/conn.php
@@ -3,10 +3,18 @@
 <!-- user = login credential -->
 <!-- dsn > database > connect to host > database name -->
 <?php
-    $host = 'localhost'; 
-    $db = 'php_assignment2_db';
-    $user = 'root';
-    $pass = '';
+    //Pesronal system database connection
+    //$host = 'localhost'; 
+    //$db = 'php_assignment2_db';
+    //$user = 'root';
+    //$pass = '';
+    //$charset = 'utf8mb4';
+
+    //remote datebase connection
+    $host = 'remotemysql.com'; 
+    $db = 'VcqeYknmDc';
+    $user = 'VcqeYknmDc';
+    $pass = 'H3pERMjdgb';
     $charset = 'utf8mb4';
 
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
